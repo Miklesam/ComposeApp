5db41bc489cef083926498b2c5bf3bea146aabd8
Miklesami <mikle.samarkin@gmail.com>
2021-01-05 12:41:38 +0300

    hilt setup

---
+++ app/src/main/java/com/miklesam/composeapp/RecipeApplication.kt
@@ -0,0 +1,9 @@
+package com.miklesam.composeapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RecipeApplication :Application(){
+
+}
---
+++ app/src/main/java/com/miklesam/composeapp/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.miklesam.composeapp.di
+
+
+import android.content.Context
+import com.miklesam.composeapp.RecipeApplication
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context): RecipeApplication {
+        return app as RecipeApplication
+    }
+
+    @Singleton
+    @Provides
+    fun provideRandomString(): String {
+        return "Hey look a random String !!!"
+    }
+
+
+}
