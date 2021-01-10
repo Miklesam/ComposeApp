394003a98ac450f4162487252cc4f32cc78ed47a
Miklesami <mikle.samarkin@gmail.com>
2021-01-10 18:33:01 +0300

    add search

--- app/src/main/java/com/miklesam/composeapp/presentation/RecipeListViewModel.kt
+++ app/src/main/java/com/miklesam/composeapp/presentation/RecipeListViewModel.kt
@@ -21,15 +21,15 @@ constructor(
     val query = mutableStateOf("Chicken")
 
     init {
-        newSearch()
+        newSearch("chicken")
     }
 
-    fun newSearch() {
+    fun newSearch(query: String) {
         viewModelScope.launch {
             val result = repository.search(
                 token = token,
                 page = 1,
-                query = "chicken"
+                query = query
             )
             recipes.value = result
         }
