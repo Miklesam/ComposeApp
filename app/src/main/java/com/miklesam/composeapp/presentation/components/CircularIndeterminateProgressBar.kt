5818b189a48a0f0ff31666ec5bbeaa4e3114dab5
Miklesami <mikle.samarkin@gmail.com>
2021-01-13 23:33:50 +0300

    constraint in compose

--- app/src/main/java/com/miklesam/composeapp/presentation/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/miklesam/composeapp/presentation/components/CircularIndeterminateProgressBar.kt
@@ -1,13 +1,14 @@
 package com.miklesam.composeapp.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -15,15 +16,36 @@ fun CircularIndeterminateProgressBar(
     isDisplayed: Boolean
 ) {
     if (isDisplayed) {
-        Row(
+
+        ConstraintLayout(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(50.dp),
-            horizontalArrangement = Arrangement.Center
+                .fillMaxSize()
         ) {
+            val (progressBar, text) = createRefs()
+
             CircularProgressIndicator(
+                modifier = Modifier.constrainAs(progressBar) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
                 color = MaterialTheme.colors.primary
             )
+            Text(
+                text = "Loading...",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = TextUnit.Companion.Sp(15)
+                ),
+                modifier = Modifier.constrainAs(text){
+                    top.linkTo(progressBar.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
         }
+
+
     }
 }
